446d47d93e6d4c973d525c3214559879042fad22
seongmin36 <whtjdals3625@gmail.com>
2026-08-27 23:49:31 +0900

    feat: Atomic Design 컴포넌트 계층 및 도메인별 폴더 구조 구성

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -3,7 +3,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { ReactNode } from "react";
-import { getQueryClient } from "@/lib/query-client";
+import { getQueryClient } from "@/lib/queryClient";
 
 /**
  * TanStack Query Provider (서버 상태).
