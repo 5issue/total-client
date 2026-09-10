e1e204c47c4e99cf50cd1794b2376d74dde0fbd5
dew2314 <dltmf2314@naver.com>
2026-09-10 17:49:07 +0900

    feat: 검색어 자동완성/연관검색어 드롭다운 추가

--- src/app/(shop)/search/page.tsx
+++ src/app/(shop)/search/page.tsx
@@ -1,17 +1,5 @@
-import { DietRecommendationBanner } from '@/components/organisms/search/DietRecommendationBanner';
-import { RecentSearchesSection } from '@/components/organisms/search/RecentSearchesSection';
-import { RecommendedKeywordsSection } from '@/components/organisms/search/RecommendedKeywordsSection';
-import { SearchPageHeader } from '@/components/organisms/search/SearchPageHeader';
-import { TrendingSearchesSection } from '@/components/organisms/search/TrendingSearchesSection';
+import { SearchPageContent } from '@/components/organisms/search/SearchPageContent';
 
 export default function SearchPage() {
-  return (
-    <div className="flex flex-1 flex-col">
-      <SearchPageHeader />
-      <DietRecommendationBanner />
-      <RecentSearchesSection />
-      <RecommendedKeywordsSection />
-      <TrendingSearchesSection />
-    </div>
-  );
+  return <SearchPageContent />;
 }
---
+++ src/components/organisms/search/SearchSuggestionsSection/index.ts
@@ -0,0 +1,1 @@
+export { SearchSuggestionsSection } from './SearchSuggestionsSection';
